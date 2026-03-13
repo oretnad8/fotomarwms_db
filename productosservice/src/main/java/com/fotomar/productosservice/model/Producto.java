32d274ca60332ef510b008c9f90fd0e1ab8cb2af
Dante Ro <or.etnad8@gmail.com>
2026-03-13 20:05:36 -0300

    actualizacion msvc

--- productosservice/src/main/java/com/fotomar/productosservice/model/Producto.java
+++ productosservice/src/main/java/com/fotomar/productosservice/model/Producto.java
@@ -15,35 +15,35 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Producto {
-    
+
     @Id
-    @Column(length = 7)
+    @Column(length = 9)
     private String sku;
-    
+
     @Column(length = 255, nullable = false)
     private String descripcion;
-    
+
     @Column(nullable = false)
     private Integer stock = 0;
-    
+
     @Column(name = "codigo_barras_individual", length = 50)
     private String codigoBarrasIndividual;
-    
+
     @Column(length = 50)
     private String lpn;
-    
+
     @Column(name = "lpn_desc", length = 255)
     private String lpnDesc;
-    
+
     @Column(name = "fecha_vencimiento")
     private LocalDate fechaVencimiento;
-    
+
     @Column(name = "vencimiento_cercano", nullable = false)
     private Boolean vencimientoCercano = false;
-    
+
     @OneToMany(mappedBy = "producto", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<ProductoUbicacion> ubicaciones = new ArrayList<>();
-    
+
     public void calcularVencimientoCercano() {
         if (fechaVencimiento != null) {
             LocalDate dosMesesDespues = LocalDate.now().plusMonths(2);
